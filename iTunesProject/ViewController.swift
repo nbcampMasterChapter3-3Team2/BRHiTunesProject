128e4f63372552756bd8a005b96c11892c49619c
RaeBaek <bmp117@naver.com>
2025-05-08 15:41:05 +0900

    setting: 프로젝트 초기세팅
    - .gitignore 파일 추가

---
+++ iTunesProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesProject
+//
+//  Created by 백래훈 on 5/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iTunesProjectTests/iTunesProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesProjectTests.swift
+//  iTunesProjectTests
+//
+//  Created by 백래훈 on 5/8/25.
+//
+
+import Testing
+@testable import iTunesProject
+
+struct iTunesProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
